fba8dd927d2a7d6db7d99b93ed9b394bc449344e
HEEYEON CHO <202015006@kyonggi.ac.kr>
2022-08-06 17:35:58 +0900

    :art: Setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'lib/UI/intro.dart';
+import 'UI/intro.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

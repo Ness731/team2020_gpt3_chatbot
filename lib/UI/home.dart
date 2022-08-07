750df425e23d4b1abea9d6d258574544ce4e87b8
sxhxrx <shr2001@naver.com>
2022-08-08 02:49:58 +0900

    update UI

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -5,6 +5,7 @@ const c2 = Colors.white;
 
 class Home extends StatefulWidget {
   String userName;
+
   Home(this.userName);
 
   @override
@@ -15,19 +16,124 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Text(widget.userName),
-            TextButton(
-              child: Text('intro 화면으로 돌아가기'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ), // 수정할 곳
+      appBar: AppBar(
+        title: Text(
+          '2020 챗봇',
+          style: TextStyle(
+            color: c2,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            color: c2,
+            iconSize: 30,
+            onPressed: () {
+              _showDialog();
+            },
+          ),
+        ],
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        backgroundColor: c1,
+        elevation: 0,
       ),
+      body: Center(
+        child: Text(widget.userName),
+      ),
+    );
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          backgroundColor: c1,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+          content: Container(
+            width: (MediaQuery.of(context).size.width) * 0.45,
+            height: (MediaQuery.of(context).size.width) * 0.25,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "정말로 대화를 끝내시겠습니까?",
+                  style: TextStyle(
+                    color: c2,
+                    fontSize: 17,
+                  ),
+                ),
+                Text(
+                  "대화 기록은 삭제됩니다.",
+                  style: TextStyle(
+                    color: c2,
+                    fontSize: 17,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 80,
+                      height: 30,
+                      margin: const EdgeInsets.only(top: 20),
+                      child: TextButton(
+                        child: Text(
+                          "예",
+                          style: TextStyle(
+                            color: c1,
+                          ),
+                        ),
+                        style: TextButton.styleFrom(
+                          backgroundColor: c2,
+                          side: BorderSide(color: c2),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    Container(width: 40),
+                    Container(
+                      width: 80,
+                      height: 30,
+                      margin: const EdgeInsets.only(top: 20),
+                      child: TextButton(
+                        child: Text(
+                          "아니오",
+                          style: TextStyle(
+                            color: c1,
+                          ),
+                        ),
+                        style: TextButton.styleFrom(
+                          backgroundColor: c2,
+                          side: BorderSide(color: c2),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GPT3 ChatBot',
-      theme: ThemeData.light(),
+      title: 'OpenAI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Color(0x90a2f0),
+          primaryColorLight: Color(0xff987654),
+          primaryColorDark: Color(0xff555555),
+          secondaryHeaderColor: Color(0xff333333),
+          backgroundColor: Color(0xff222222),
+          textTheme: const TextTheme(
+            bodyText1: TextStyle(color: Color(0xff131313), fontSize: 20),
+            bodyText2: TextStyle(color: Color(0xff313131), fontSize: 20),
+          ),
+          scaffoldBackgroundColor: Colors.white
+      ),
       home: Intro(),
     );
   }
